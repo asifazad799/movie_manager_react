ec081c21f11384d8eb6a074f25b71f69840c510e
Asif Azad <asif.azad@neo-world.com>
2023-12-13 01:59:24 +0530

    add - movie card

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import DeleteOutlineRoundedIcon from "@mui/icons-material/DeleteOutlineRounded";
+import { Checkbox } from "@mui/material";
+import CheckCircleOutlineRoundedIcon from "@mui/icons-material/CheckCircleOutlineRounded";
+import RadioButtonUncheckedRoundedIcon from "@mui/icons-material/RadioButtonUncheckedRounded";
 
 function MovieCards({ title, backdrop, poster }) {
+  const [hover, setHover] = useState(false);
+
   return (
     <div
       className="login-form d-flex movieListCard"
       style={{ backgroundImage: `url(${poster})` }}
+      onMouseEnter={() => {
+        setHover((pre) => !pre);
+      }}
+      onMouseLeave={() => {
+        setHover((pre) => !pre);
+      }}
     >
+      {hover && (
+        <div className="hoverLayer">
+          <DeleteOutlineRoundedIcon className="deleteIcon" color="red" />
+
+          <div className="watchToggleSection">
+            <p className="defaultFontFam">Wtched</p>
+            <Checkbox
+              icon={
+                <RadioButtonUncheckedRoundedIcon className="checkBoxIcon notWatched" />
+              }
+              checkedIcon={
+                <CheckCircleOutlineRoundedIcon className="checkBoxIcon watched" />
+              }
+            />
+          </div>
+        </div>
+      )}
       {/* <img src={backdrop} /> */}
       {/* {title} */}
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,6 +26,96 @@ function Home() {
       slug: "the-dark-knight-2008",
       title: "The Dark Knight",
     },
+    {
+      backdrop:
+        "https://wookie.codesubmit.io/static/backdrops/a9d94d6e-4cab-44a9-8eec-d44ad6332b6d.jpg",
+      cast: ["John Travolta", "Uma Thurman", "Samuel L. Jackson"],
+      classification: "18+",
+      director: "Quentin Tarantino",
+      genres: ["Crime", "Drama"],
+      id: "a9d94d6e-4cab-44a9-8eec-d44ad6332b6d",
+      imdb_rating: 8.9,
+      length: "2h 34min",
+      overview:
+        "A burger-loving hit man, his philosophical partner, a drug-addled gangster's moll and a washed-up boxer converge in this sprawling, comedic crime caper. Their adventures unfurl in three stories that ingeniously trip back and forth in time.",
+      poster:
+        "https://wookie.codesubmit.io/static/posters/a9d94d6e-4cab-44a9-8eec-d44ad6332b6d.jpg",
+      released_on: "1994-09-10T00:00:00",
+      slug: "pulp-fiction-1994",
+      title: "Pulp Fiction",
+    },
+    {
+      backdrop:
+        "https://wookie.codesubmit.io/static/backdrops/4ec83f0e-eede-4453-8f87-461525e21f6e.jpg",
+      cast: ["Liam Neeson", "Ralph Fiennes", "Ben Kingsley"],
+      classification: "18+",
+      director: "Steven Spielberg",
+      genres: ["Biography", "Drama", "History"],
+      id: "4ec83f0e-eede-4453-8f87-461525e21f6e",
+      imdb_rating: 8.9,
+      length: "3h 15min",
+      overview:
+        "The true story of how businessman Oskar Schindler saved over a thousand Jewish lives from the Nazis while they worked as slaves in his factory during World War II.",
+      poster:
+        "https://wookie.codesubmit.io/static/posters/4ec83f0e-eede-4453-8f87-461525e21f6e.jpg",
+      released_on: "1993-12-15T00:00:00",
+      slug: "schindlers-list-1993",
+      title: "Schindler's List",
+    },
+    {
+      backdrop:
+        "https://wookie.codesubmit.io/static/backdrops/f3d91837-a2ff-4250-99b0-e8c9c036a23a.jpg",
+      cast: ["Shameik Moore", "Jake Johnson", "Hailee Steinfeld"],
+      classification: "7+",
+      director: ["Bob Persichetti", "Peter Ramsey", "Rodney Rothman"],
+      genres: ["Animation", "Action", "Adventure"],
+      id: "f3d91837-a2ff-4250-99b0-e8c9c036a23a",
+      imdb_rating: 8.5,
+      length: "1h 57min",
+      overview:
+        'Miles Morales is juggling his life between being a high school student and being a spider-man. When Wilson "Kingpin" Fisk uses a super collider, others from across the Spider-Verse are transported to this dimension.',
+      poster:
+        "https://wookie.codesubmit.io/static/posters/f3d91837-a2ff-4250-99b0-e8c9c036a23a.jpg",
+      released_on: "2018-12-06T00:00:00",
+      slug: "spider-man-into-the-spider-verse-2018",
+      title: "Spider-Man: Into the Spider-Verse",
+    },
+    {
+      backdrop:
+        "https://wookie.codesubmit.io/static/backdrops/9a70e529-9070-4a2f-963c-c5bb253cc721.jpg",
+      cast: ["Robert Downey Jr.", "Chris Hemsworth", "Mark Ruffalo"],
+      classification: "13+",
+      director: ["Anthony Russo", "Joe Russo"],
+      genres: ["Action", "Adventure", "Sci-Fi"],
+      id: "9a70e529-9070-4a2f-963c-c5bb253cc721",
+      imdb_rating: 8.5,
+      length: "2h 29min",
+      overview:
+        "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
+      poster:
+        "https://wookie.codesubmit.io/static/posters/9a70e529-9070-4a2f-963c-c5bb253cc721.jpg",
+      released_on: "2018-04-25T00:00:00",
+      slug: "avengers-infinity-war-2018",
+      title: "Avengers: Infinity War",
+    },
+    {
+      backdrop:
+        "https://wookie.codesubmit.io/static/backdrops/4d949e14-b08b-47fb-bab0-22c732dbedf3.jpg",
+      cast: ["Harrison Ford", "Karen Allen", "Paul Freeman"],
+      classification: "7+",
+      director: "Steven Spielberg",
+      genres: ["Action", "Adventure"],
+      id: "4d949e14-b08b-47fb-bab0-22c732dbedf3",
+      imdb_rating: 8.5,
+      length: "1h 55min",
+      overview:
+        "When Dr. Indiana Jones – the tweed-suited professor who just happens to be a celebrated archaeologist – is hired by the government to locate the legendary Ark of the Covenant, he finds himself up against the entire Nazi regime.",
+      poster:
+        "https://wookie.codesubmit.io/static/posters/4d949e14-b08b-47fb-bab0-22c732dbedf3.jpg",
+      released_on: "1981-06-12T00:00:00",
+      slug: "raiders-of-the-lost-ark-1981",
+      title: "Raiders of the Lost Ark",
+    },
     { title: "titanic" },
     { title: "titanic" },
     { title: "titanic" },
@@ -57,7 +147,7 @@ function Home() {
           <Grid container spacing={1}>
             {data?.map((movie) => {
               return (
-                <Grid xs={12} sm={6} md={4} item>
+                <Grid key={movie?.title} xs={12} sm={6} md={4} item>
                   <MovieCards {...movie} />
                 </Grid>
               );
