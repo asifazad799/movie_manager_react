c54dcc093846f23f7eef923e239f9a3d816fc9dc
asifazad799 <asifazad799@gmail.com>
2024-04-30 13:07:15 +0400

    test - 2 -  deploy pipeline

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -33,7 +33,7 @@ export function LoginForm({ customHandleSubmit }) {
 
   return (
     <div className="login-form d-flex">
-      <p className="defaultFont">{"Log In Asif"}</p>
+      <p className="defaultFont">{"Log In"}</p>
       <form className="form" onSubmit={handleSubmit}>
         <img src={lock} className="lockIcon" />
 
