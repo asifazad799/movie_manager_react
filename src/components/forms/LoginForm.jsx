85480cc051b05a82787e79ecb081c2bbe5528951
asifazad799 <asifazad799@gmail.com>
2024-06-16 00:07:26 +0530

    checking update triger

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -33,7 +33,7 @@ export function LoginForm({ customHandleSubmit }) {
 
   return (
     <div className="login-form d-flex">
-      <p className="defaultFont">{"Login Asif Azad"}</p>
+      <p className="defaultFont">{"Login"}</p>
       <form className="form" onSubmit={handleSubmit}>
         <img src={lock} className="lockIcon" />
 
