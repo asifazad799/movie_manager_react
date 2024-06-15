7ed91aa2346ffb5d2a81c305caf74a0d5b4c0e4c
asifazad799 <asifazad799@gmail.com>
2024-06-16 00:34:59 +0530

    checking update triger

--- src/main.jsx
+++ src/main.jsx
@@ -35,14 +35,11 @@ if ("serviceWorker" in navigator) {
                 console.log("New content is available; please refresh.");
                 if (confirm("New version available. Do you want to reload?")) {
                   // window.location.reload(true);
-                  let newHref = String(window.location.search);
-
-                  if (newHref.includes("?")) {
-                    newHref = newHref.slice(0, newHref.indexOf("?"));
-                  }
 
                   window.location.href =
-                    newHref + "?cache-bust=" + new Date().getTime();
+                    window.location.href.slice(0, newHref.indexOf("?")) +
+                    "?cache-bust=" +
+                    new Date().getTime();
                 }
               }
             }
