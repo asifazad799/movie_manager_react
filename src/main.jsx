b3eb98a4a0f0da414d35015d0c0d016e353b08b0
asifazad799 <asifazad799@gmail.com>
2024-06-16 00:05:58 +0530

    checking update triger

--- src/main.jsx
+++ src/main.jsx
@@ -37,10 +37,6 @@ if ("serviceWorker" in navigator) {
                     window.location.href +
                     "?cache-bust=" +
                     new Date().getTime();
-
-                  // registration.active.postMessage({
-                  //   type: "CLEAR_CACHE_AND_RELOAD",
-                  // });
                 }
               }
             }
@@ -50,40 +46,5 @@ if ("serviceWorker" in navigator) {
       .catch((error) => {
         console.log("Service Worker registration failed:", error);
       });
-
-    navigator.serviceWorker.addEventListener("controllerchange", () => {
-      window.location.reload();
-    });
   });
 }
-
-// Register the service worker with Workbox
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     const wb = new Workbox("/service-worker.js");
-
-//     wb.addEventListener("installed", (event) => {
-//       console.log("New Version is available:");
-//       if (event.isUpdate) {
-//         if (confirm("New content is available; please refresh.")) {
-//           window.location.reload();
-//         }
-//       }
-//     });
-
-//     wb.addEventListener("waiting", (event) => {
-//       console.log("New Version is available:");
-//       if (confirm("New version available. Do you want to reload?")) {
-//         wb.messageSW({ type: "SKIP_WAITING" });
-//       }
-//     });
-
-//     wb.register()
-//       .then((registration) => {
-//         console.log("Service Worker registered with Workbox:", registration);
-//       })
-//       .catch((error) => {
-//         console.log("Service Worker registration failed with Workbox:", error);
-//       });
-//   });
-// }
