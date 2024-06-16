ee7268f7ad44f8e2e5da3e624792e633e33842ec
asifazad799 <asifazad799@gmail.com>
2024-06-17 01:09:36 +0530

    service worker update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Workbox } from "workbox-window";
-import * as serviceWorkerRegistration from "./service-worker-reg";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
