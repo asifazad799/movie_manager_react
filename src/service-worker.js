19bad382f376aca85060b3535f7ceac6a663fa58
asifazad799 <asifazad799@gmail.com>
2024-06-16 16:17:44 +0530

    service worker update

--- src/service-worker.js
+++ src/service-worker.js
@@ -89,6 +89,37 @@ self.addEventListener("message", (event) => {
   }
 });
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("static-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/assets/*",
+        // Add other UI files here
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((response) => {
+          // Cache the response for future requests
+          caches.open("api-cache").then((cache) => {
+            cache.put(event.request, response.clone());
+          });
+
+          return response;
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "CLEAR_CACHE_AND_RELOAD") {
     caches
